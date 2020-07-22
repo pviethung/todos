3a61be1312435a22eca0112e4b6cab0183b56d43
hungxu <pv2hung97@gmail.com>
2020-07-22 16:10:41 +0700

    Updates

---
+++ precache-manifest.dcf914982e9ce41326911f325bfca9e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df542c67cc176778a9dc01fab108aa1",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "380b3263982459f11b90",
+    "url": "/todos/static/css/main.23bec056.chunk.css"
+  },
+  {
+    "revision": "4c4cbc000e4e6869e60a",
+    "url": "/todos/static/js/2.9c47011d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todos/static/js/2.9c47011d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380b3263982459f11b90",
+    "url": "/todos/static/js/main.178cd42b.chunk.js"
+  },
+  {
+    "revision": "91cb65b14c89b80d78fc",
+    "url": "/todos/static/js/runtime-main.83f23780.js"
+  },
+  {
+    "revision": "7e650390e7217d3492caac409d68cd9b",
+    "url": "/todos/static/media/img1.7e650390.svg"
+  }
+]);
